b4125a8e2f196877b60c9668f98b0c1bef0e899f
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-14 09:30:59 +0100

    append name in html

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -6,32 +6,52 @@ Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
 - per i multipli di 5 stampi “Buzz”
 - per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Come faccio a sapere se un numero è divisibile per un altro?
-Abbiamo usato un operatore  nei giorni scorsi che possiamo riusare in questo
-esercizio?
+Bonus 1: crea un container nella DOM, aggiungendo con append un elemento html con il numero e la stringa corretta da mostrare
 
 */
 
 
 /* strumenti: 
-
+class.list.add <---- cerca 
 */
+const conatainerEl = document.querySelector('.container')
 
-const ulElement = document.createElement("ul");
+
+for (let i = 1; i <= 100; i++) {
+    const divEl =document.createElement('div')
+    //per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+        divEl.append('Fizzbuzz')
+        /* - per i multipli di 3 stampi “Fizz” al posto del numero */
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+        divEl.append('Fizz')
+        /* - per i multipli di 5 stampi “Bizz” al posto del numero */
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+        divEl.append('Buzz')
+    }else{
+        console.log(i)
+        divEl.append(i)
+    }
+conatainerEl.append(divEl)
+}
+//Bonus 1: crea un container nella DOM, aggiungendo con append un elemento html con il numero e la stringa corretta da mostrare
+
+
+/* const ulElement = document.createElement("ul");
 console.log(ulElement)
 
 const bodyElement = document.querySelector("body")
-bodyElement.append(ulElement)
+bodyElement.append(ulElement) */
 
-for (let i = 0; i < 101; i++) {
 
-    console.log(i);
-    const liElement = document.createElement("li")
-    console.log(liElement)
-    liElement.append(i)
-    console.log(liElement)
-    ulElement.append(liElement)
-    console.log(ulElement)
-
-}
+/* console.log(i);
+const liElement = document.createElement("li")
+console.log(liElement)
+liElement.append(i)
+console.log(liElement)
+ulElement.append(liElement)
+console.log(ulElement)
+ */
